7ff07b961cbbeccb3d5796fabc2c1e5d94787e0d
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-10-21 12:17:36 +0300

    feat; display curernt login user info

--- front-end/src/app/features/components/account-popover/account-popover.component.ts
+++ front-end/src/app/features/components/account-popover/account-popover.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import User from '../../types/user';
 
 @Component({
   selector: 'app-account-popover',
   templateUrl: './account-popover.component.html',
-  styleUrls: ['./account-popover.component.scss']
+  styleUrls: ['./account-popover.component.scss'],
 })
-export class AccountPopoverComponent implements OnInit {
-
-  constructor(public authService: AuthService) { }
+export class AccountPopoverComponent implements OnInit, AfterViewInit {
+  user = null;
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
   }
 
+  ngAfterViewInit() {
+    this.user = JSON.parse(localStorage.getItem('user'));
+  }
 }
