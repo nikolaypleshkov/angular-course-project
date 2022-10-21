4c6cf756af45c6fd57d7c55c8de2e7b0f50a1b6a
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-10-21 12:18:19 +0300

    feat: show error messages from response in register

--- front-end/src/app/features/components/register-form/register-form.component.ts
+++ front-end/src/app/features/components/register-form/register-form.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import {
+  UntypedFormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -18,11 +24,10 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
-
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent implements OnInit {
   emailFormControl = new UntypedFormControl('', [
@@ -32,20 +37,50 @@ export class RegisterFormComponent implements OnInit {
 
   matcher = new MyErrorStateMatcher();
   loading: boolean = false;
+  
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  constructor(private router: Router, public authService: AuthService) {}
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
+  openSnackBar(message: string) {
+    this.snackBar.open(message, '', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
 
   handleSubmit(email: string, full_name: string, password: string): void {
     this.loading = true;
-    this.authService.SignUp(email, full_name, password).then(() => {
-      this.loading = false;
-    }
-    ).catch((error) => {
-      this.loading = false;
-      window.alert(error.message)
-    }
-    )
+    this.authService
+      .SignUp(email, full_name, password)
+      .then(() => {
+        this.loading = false;
+      })
+      .catch((error) => {
+        this.loading = false;
+        switch(error) {
+          case 'auth/email-already-in-use':
+            this.openSnackBar('Email already in use');
+            break;
+          case 'auth/invalid-email':
+            this.openSnackBar('Invalid email');
+            break;
+          case 'auth/operation-not-allowed':
+            this.openSnackBar('Operation not allowed');
+            break;
+          case 'auth/weak-password':
+            this.openSnackBar('Weak password');
+            break;
+          default:
+            this.openSnackBar('Something went wrong');
+            
+        }
+      });
   }
 }
