bc931c2867d5e53b18eed4b1a2619390a75b8f3b
Nikolay Pleshkov <pleshkovn1kk@gmail.com>
2022-11-04 12:01:26 +0200

    feat: added guards and the new pages for teacher

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './features/guards/auth.guard';
+import { TeacherGuard } from './features/guards/teacher.guard';
 import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 import { CalendarComponent } from './views/calendar/calendar.component';
 import { ClassComponent } from './views/class/class.component';
@@ -7,9 +9,9 @@ import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { MyClassesComponent } from './views/my-classes/my-classes.component';
 import { MyTasksComponent } from './views/my-tasks/my-tasks.component';
-import { MyTsksComponent } from './views/my-tsks/my-tsks.component';
 import { RegisterComponent } from './views/register/register.component';
 import { SettingsComponent } from './views/settings/settings.component';
+import { TaskComponent } from './views/task/task.component';
 
 const routes: Routes = [
   {
@@ -19,27 +21,49 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: 'settings',
         component: SettingsComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: 'my-classes',
         component: MyClassesComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: 'my-tasks',
         component: MyTasksComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: 'calendar',
         component: CalendarComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: 'class/:id',
         component: ClassComponent,
+        canActivate: [AuthGuard],
       },
+      {
+        path: 'task/:id',
+        component: TaskComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'create-class',
+        component: ClassComponent,
+        canActivate: [AuthGuard, TeacherGuard],
+      },
+      {
+        path: 'create-task',
+        component: TaskComponent,
+        canActivate: [AuthGuard, TeacherGuard],
+      }
+      
     ],
   },
   {
